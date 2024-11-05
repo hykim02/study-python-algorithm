0b9a44f8deaea23bea4c07f8eea4b92e68c3d3ad
hykim02 <hykim4435@naver.com>
2024-11-06 01:57:32 +0900

    [feat] 시각

---
+++ Implementation/시각.py
@@ -0,0 +1,13 @@
+# 4-2
+h = int(input())
+count = 0
+
+# h + 1인 이유 -> h가 5이면 0시부터 5시까지 포함해야 하는데
+# range 범위는 매개변수 본인은 포함하지 않기 때문
+for i in range(h + 1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+
+print(count)
