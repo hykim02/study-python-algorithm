8d5901e176c97aa6a61616a4507b457c875e403c
hykim02 <hykim4435@naver.com>
2024-11-13 23:22:32 +0900

    [feat] 1이 될 때까지 recode

--- Greedy/1이 될 때까지1.py
+++ Greedy/1이 될 때까지1.py
@@ -1,4 +1,24 @@
 # 3-5
+# 나의 답안
+n, k = map(int, input().split())
+count = 0
+
+while(n >= k):
+    # n이 k로 나누어 떨어지는 경우
+    if (n % k == 0):
+        n //= k
+        count += 1
+    else: # 나누어 떨어지지 않는 경우
+        count += (n % k) # 1을 빼야 하는 횟수
+        n = ((n // k) * k)
+
+if(n < k):
+    count += (n-1) # n에서 (n-1)을 빼주면 1이 됨
+
+print(count)
+
+# -------------------------------------------------------------
+# 예제 답안 1
 n, k = map(int, input().split())
 count = 0
 
