4bd6c88ca59c1f0f7b2df8aaaa70615cf6ac0827
hykim02 <hykim4435@naver.com>
2024-11-14 22:29:40 +0900

    [feat] 상하좌우 recode

--- Implementation/상하좌우.py
+++ Implementation/상하좌우.py
@@ -1,4 +1,34 @@
 # 4-1
+# 나의 답안
+# 공간 크기 입력받기
+n = int(input())
+move_plans = list(input().split())
+
+# 이동 방향 정의 
+move_types = ['L', 'R', 'U', 'D']
+dx = [0, 0, -1, 1]
+dy = [-1, 1, 0, 0]
+
+# 출발 위치는 항상 (1, 1)
+x, y = 1, 1
+
+# 이동 계획 수행
+for move_plan in move_plans:
+    idx = move_types.index(move_plan) # 문자 인덱스 찾기
+    nx = x + dx[idx]
+    ny = y + dy[idx]
+
+    # 정사각형을 벗어나는지 확인
+    if (nx >= 1 and nx <= n and ny >= 1 and ny <= n):
+        x = nx
+        y = ny
+    else:
+        continue
+
+print(x, y)
+
+# ----------------------------------------------------------------
+# 예제 답안
 n = int(input())
 x, y = 1, 1
 plans = input().split() # 이동 방향이 리스트로 생성됨
