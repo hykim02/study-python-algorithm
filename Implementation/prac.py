added975b720ac6e4d26f6634a58b25f3290c1f8
hykim02 <hykim4435@naver.com>
2024-11-19 16:28:53 +0900

    [feat] 왕실의 나이트 re-code

--- Implementation/prac.py
+++ Implementation/prac.py
@@ -2,8 +2,16 @@ a = [1,2]
 b = 3
 c = 's'
 d = {"a":1, "b":3}
+e = "hello"
+f = (2,1)
+g = [(2,1), (1,0)]
 
 print(type(a))
 print(type(b))
 print(type(c))
 print(type(d))
+print(e[3])
+print(type(f))
+print(f[0])
+print(type(g))
+print(g[0][1])
