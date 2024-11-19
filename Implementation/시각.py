fe54866989fd20cba39b435f20a98115d4a87e6e
hykim02 <hykim4435@naver.com>
2024-11-19 15:40:11 +0900

    [feat] 시각 re-code

--- Implementation/시각.py
+++ Implementation/시각.py
@@ -1,4 +1,18 @@
 # 4-2
+# 나의답안
+n = int(input())
+count = 0
+
+for i in range(n + 1):
+    for j in range(60):
+        for k in range(60):
+            if '3' in str(i) + str(j) + str(k):
+                count += 1
+                
+print(count)
+
+# ------------------------------------------------------------------
+# 예시답안
 h = int(input())
 count = 0
 
