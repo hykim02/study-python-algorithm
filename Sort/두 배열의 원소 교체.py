6e5e281044778e544c4261e6bfa127b3de62d37b
hykim02 <hykim4435@naver.com>
2025-05-21 19:40:13 +0900

    sort-두 배열의 원소 교체

---
+++ Sort/두 배열의 원소 교체.py
@@ -0,0 +1,14 @@
+N, K = map(int, input().split())
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+A.sort() # 오름차순
+B.sort(reverse=True) # 내림차순 
+
+for i in range(K):
+    if A[i] < B[i]:
+        A[i], B[i] = B[i], A[i]
+    else:
+        break
+
+print(sum(A))
